515ace046af130a0f71a26880888a61fd35843f4
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-08-19 14:39:47 +0530

    Update TwoDArrayDem.java

--- TwoDArrayDem.java
+++ TwoDArrayDem.java
@@ -1,30 +1,42 @@
 import java.util.Scanner;
 
 public class TwoDArrayDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter the number of row : ");
-	    int row= sc.nextInt();
-	    System.out.println("Enter the number of col : ");
-	    int col= sc.nextInt();
-	    int [][]array =new  int[row][col];
-	    System.out.println("Enter the  elements : ");
-	    for(int i=0;i<row;i++) {
-	    	for(int j=0;j<col;j++) 
-	    		array[i][j]=sc.nextInt();
-	    }
-	    int [][]transpose =new  int[row][col];
-	    for(int i=0;i<row;i++) {
-	    	for(int j=0;j<col;j++) 
-	    		transpose[j][1]=array[i][j];
-	    }
-	    System.out.println("The  elements are : ");
-	    for(int i=0;i<row;i++) {
-	    	for(int j=0;j<col;j++) 
-	    		System.out.println(transpose[i][j]+"\t");
-	    }
+        // Input matrix dimensions
+        System.out.print("Enter the number of rows: ");
+        int row = sc.nextInt();
+        System.out.print("Enter the number of cols: ");
+        int col = sc.nextInt();
 
-	}
+        int[][] array = new int[row][col];
 
+        // Input elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        // Transpose matrix (swap row & col)
+        int[][] transpose = new int[col][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                transpose[j][i] = array[i][j];
+            }
+        }
+
+        // Print transpose
+        System.out.println("Transpose Matrix:");
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print(transpose[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
