3f0f3ca6e160bd532ae0516e34207bdb3eff1e9e
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-08-19 15:00:32 +0530

    Update Transpose.java

--- Transpose.java
+++ Transpose.java
@@ -8,15 +8,15 @@ import java.util.Scanner;
 
 public class Transpose {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
 
      
         System.out.print("Enter the number of rows: ");
-        int row = sc.nextInt();
+        int row=sc.nextInt();
         System.out.print("Enter the number of cols: ");
-        int col = sc.nextInt();
+        int col=sc.nextInt();
 
-        int[][] array = new int[row][col];
+        int[][] array=new int[row][col];
 
         System.out.println("Enter the elements:");
         for (int i=0;i<row;i++){
@@ -26,7 +26,7 @@ public class Transpose {
         }
 
  
-        int[][] transpose = new int[col][row];
+        int[][] transpose=new int[col][row];
         for (int i=0;i<row;i++){
             for (int j=0;j<col;j++){
                 transpose[j][i]=array[i][j];
